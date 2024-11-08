7b0680288dd04ed97ba37b27fd045eb44765c579
SdSingh10 <133514216+SdSingh10@users.noreply.github.com>
2024-11-08 11:16:14 +0530

    Updated@8nov

--- movie-reviews/backend/api/movies.route.js
+++ movie-reviews/backend/api/movies.route.js
@@ -5,10 +5,11 @@ import ReviewsController from './reviews.controller.js'
 const router = express.Router()
 
 router.route('/').get(MoviesController.apiGetMovies)
+
 router
     .route("/review")
-    .post("ReviewsController.apiPostReview")
-    .put("ReviewsController.apiUpdateReview")
-    .delete("ReviewsController.apiDeleteReview")
+    .post(ReviewsController.apiPostReview)
+    .put(ReviewsController.apiUpdateReview)
+    .delete(ReviewsController.apiDeleteReview)
     
 export default router
--- movie-reviews/backend/api/reviews.controller.js
+++ movie-reviews/backend/api/reviews.controller.js
@@ -4,10 +4,10 @@ export default class ReviewsController{
     static async apiPostReview(req, res, next){
         try{
             const movieId = req.body.movie_id
-            const review = req.body.review
+            const review = req.body.text
             const userInfo = {
                 name: req.body.name,
-                _id: req.body.user_id
+                _id: req.body.user_id,
             }
             
             const date = new Date()
@@ -24,16 +24,49 @@ export default class ReviewsController{
         }
     }
 
-    static async apiUPdateReview(req, res, next){
+    static async apiUpdateReview(req, res, next){
         try{
             const reviewId = req.body.review_id;
             const review = req.body.review;
 
             const date = new Date();
-            
+
+            const ReviewResponse = await ReviewsDAO.apiUpdateReview(
+                reviewId,
+                req.body.user_id,
+                review,
+                date
+            )
+
+            var { error } = ReviewResponse
+            if(error){
+                res.status.json({error})
+            }
+
+            if(ReviewResponse.modifiedCount === 0){
+                throw new Error ("unable toupdate review. User may not be original poster")
+            }
+            res.json({ status : "success" })
         }
         catch{
+            res.status(500).json({error: e.message})
+        }
+    }
 
+    static async apiDeleteReview(req, res, next){
+        try{
+            const reviewId = req.body.review_id;
+            const userId = req.body.user_id;
+
+            const ReviewResponse = await ReviewsDAO.apiDeleteReview(
+                reviewId,
+                userId,
+            )
+
+            res.json({ status: "success" })
+        }
+        catch(e){
+            res.status(500).json({ error: e.message })
         }
     }
 }
